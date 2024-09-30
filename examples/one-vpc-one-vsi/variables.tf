34eb2721609a53e9a955011f516169ef4aa3f01a
Todd Giguere <todd.giguere@ibm.com>
2024-09-30 14:56:30 -0400

    many more prefix validations removed

--- examples/one-vpc-one-vsi/variables.tf
+++ examples/one-vpc-one-vsi/variables.tf
@@ -13,10 +13,7 @@ variable "prefix" {
   type        = string
   default     = "extra-simple"
 
-  validation {
-    error_message = "Prefix must begin with a lowercase letter and contain only lowercase letters, numbers, and - characters. Prefixes must end with a lowercase letter or number and be 16 or fewer characters."
-    condition     = can(regex("^([a-z]|[a-z][-a-z0-9]*[a-z0-9])$", var.prefix)) && length(var.prefix) <= 16
-  }
+
 }
 
 variable "region" {
--- patterns/dynamic_values/config_modules/vpc/variables.tf
+++ patterns/dynamic_values/config_modules/vpc/variables.tf
@@ -6,10 +6,7 @@ variable "prefix" {
   description = "A unique identifier for resources. Must begin with a letter and end with a letter or number. This prefix will be prepended to any resources provisioned by this template. Prefixes must be 16 or fewer characters."
   type        = string
 
-  validation {
-    error_message = "Prefix must begin and end with a letter and contain only letters, numbers, and - characters. Prefixes must end with a letter or number and be 16 or fewer characters."
-    condition     = can(regex("^([A-z]|[a-z][-a-z0-9]*[a-z0-9])$", var.prefix)) && length(var.prefix) <= 16
-  }
+
 }
 
 variable "vpcs" {
